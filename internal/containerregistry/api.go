ed914427ae78345f8ea333412f9020e5b43baecc
Alex Solanos <solanosalex@gmail.com>
2020-08-19 19:18:34 +0300

    Get all images in registry through goroutines

--- internal/containerregistry/api.go
+++ internal/containerregistry/api.go
@@ -2,6 +2,7 @@ package containerregistry
 
 /**
  * Api docs at https://docs.docker.com/registry/spec/api/
+ * These api methods slightly process the responses before returning
  */
 
 import (
@@ -17,6 +18,15 @@ type catalogDTO struct {
 	// Next is used for pagination purposes, it contains the next URL we need to GET for the next page
 	Next         string
 	Repositories []Repository
+}
+
+// listTagsDTO is the Data Transfer Object for the list tags api call
+type listTagsDTO struct {
+	// Manifest keys are the image digest
+	Manifest map[string]ContainerImage
+	Name     string
+	Tags     []string
+	Next     string
 }
 
 // Repository describes a url where the registry holds docker images
@@ -53,4 +63,40 @@ func (gcrClient GCRClient) getRepositories() []Repository {
 	}
 
 	return repositories
+}
+
+func (gcrClient GCRClient) listTags(repository Repository) []ContainerImage {
+	containerImages := []ContainerImage{}
+
+	listTagsResp := listTagsDTO{
+		Next: "/" + string(repository) + "/tags/list", // initial request
+	}
+
+	for {
+		bodyBytes := gcrClient.getRequestTo(listTagsResp.Next)
+
+		listTagsResp = listTagsDTO{}
+		err := json.Unmarshal(bodyBytes, &listTagsResp)
+
+		for digest, image := range listTagsResp.Manifest {
+			image.Digest = digest
+			containerImages = append(containerImages, image)
+		}
+
+		if err != nil {
+			log.Fatalf("Invalid api call response (%v): %v", string(bodyBytes), err.Error())
+		}
+
+		if listTagsResp.Next == "" { // no more pages to GET
+			break
+		} else { // more pages to GET
+			// remove the prefix of the link as our gcr client works with suffixes
+			listTagsResp.Next = stringutil.TrimLeftChars(listTagsResp.Next, len(gcrClient.getBaseURL()))
+			if listTagsResp.Next[0] != '/' {
+				listTagsResp.Next = "/" + listTagsResp.Next
+			}
+		}
+	}
+
+	return containerImages
 }
--- internal/containerregistry/container_registry.go
+++ internal/containerregistry/container_registry.go
@@ -1,9 +1,9 @@
 package containerregistry
 
 import (
+	"fmt"
+	"math"
 	"net/http"
-
-	log "github.com/sirupsen/logrus"
 )
 
 // GCRClient is used for creating Google Container Registry clients
@@ -15,7 +15,13 @@ type GCRClient struct {
 
 // ContainerImage contains all the data that are relevant to an image on the registry
 type ContainerImage struct {
-	Link string
+	ImageSizeBytes string
+	LayerID        string `json:"layerId"`
+	MediaType      string
+	Tag            []string
+	TimeCreatedMs  string
+	TimeUploadedMs string
+	Digest         string
 }
 
 // MakeGCRClient builds a new GCRClient instance, adding the missing default values e.g. http client
@@ -25,15 +31,38 @@ func MakeGCRClient(client GCRClient) GCRClient {
 	return gcrClient
 }
 
-// GetAllImages returns all the images in a docker container registry
+func (gcrClient GCRClient) imagesRepoFetchWorker(repositories <-chan Repository, containerImages chan<- []ContainerImage) {
+	for job := range repositories {
+		repoImages := gcrClient.listTags(job)
+		containerImages <- repoImages
+	}
+}
+
+// GetAllImages returns all the images in a docker container registry by spawning multiple workers to make it go faster
 func (gcrClient GCRClient) GetAllImages() []ContainerImage {
 	repositories := gcrClient.getRepositories()
 
-	log.Info("Catalog contains", len(repositories), "repos")
+	repositoriesChannel := make(chan Repository, len(repositories))
+	containerImagesChannel := make(chan []ContainerImage, len(repositories))
 
-	if len(repositories) == 0 {
-		return nil
+	// spawn max 40 goroutines, if repos are less than 40, try to GET them all concurrently
+	workersNum := int(math.Min(40, float64(len(repositories))))
+
+	for i := 1; i <= workersNum; i++ {
+		go gcrClient.imagesRepoFetchWorker(repositoriesChannel, containerImagesChannel)
 	}
 
-	return nil
+	for _, repo := range repositories {
+		repositoriesChannel <- repo
+	}
+
+	// after all the channels have been filled, it's time to read from them now
+	allImages := []ContainerImage{}
+	for range repositories {
+		allImages = append(allImages, <-containerImagesChannel...)
+	}
+
+	fmt.Println("All images' length is", len(allImages))
+
+	return allImages
 }
